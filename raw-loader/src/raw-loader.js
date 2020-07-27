0360310e65e7a500ab65447c5f71310d1515da24
wenlongzhu <1573511441@qq.com>
2020-07-27 21:44:25 +0800

    feature:异步读取文件，并实现传参

--- raw-loader/src/raw-loader.js
+++ raw-loader/src/raw-loader.js
@@ -1,12 +1,19 @@
 const loaderUtils = require('loader-utils')
+const fs = require('fs')
+const path = require('path')
 
 module.exports = function (source) {
   const { name } = loaderUtils.getOptions(this)
-  console.log('name', name)
+  // console.log('name', name)
+  const callback = this.async()
   const json = JSON.stringify(source)
     .replace('zhu', 'dragon')
     .replace(/\u2028/g, '\\u2028')
     .replace(/\u2029/g, '\\u2029')
+  
+  fs.readFile(path.join(__dirname, './async.txt'), 'utf-8',(err, result) => {
+    callback(null,result)
+  })
 
 
   // 同步处理错误的方式
@@ -16,6 +23,6 @@ module.exports = function (source) {
 
   // 同步传递错误的方式2
   // this.callback(new Error('Error2'),json)
-  this.callback(null,json,2,3,4) // 传递多个值
+  // this.callback(null,json,2,3,4) // 传递多个值
 
 }
